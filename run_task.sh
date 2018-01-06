0256c2fb64acc92a4c4bdafee8ffca2610e08700
hyyc116 <hyyc116@gmail.com>
2018-01-06 16:52:09 +0800

    add

--- run_task.sh
+++ run_task.sh
@@ -1,20 +1,20 @@
 echo 'jobs dmas'
-python search_google_trends.py 'jobs' DMAs 2> jobs_dma.log
+python search_google_trends.py 'jobs' DMAs 1> jobs_dma.log 2> jobs_dma.log
 echo 'jobs state'
-python search_google_trends.py 'jobs' state 2> jobs_state.log
+python search_google_trends.py 'jobs' state 1> jobs_state.log 2> jobs_state.log
 
 echo 'skills dmas'
 
-python search_google_trends.py 'skills' DMAs 2> skills_dma.log
+python search_google_trends.py 'skills' DMAs 1> skills_dma.log 2> skills_dma.log
 
 echo 'skills state'
 
-python search_google_trends.py 'skills' state 2> skills_state.log
+python search_google_trends.py 'skills' state 1> skills_state.log 2> skills_state.log
 
 echo 'terms dmas'
 
-python search_google_trends.py terms DMAs 2> terms_dma.log
+python search_google_trends.py terms DMAs 1> terms_dma.log 2> terms_dma.log
 
 echo 'terms state'
 
-python search_google_trends.py terms state 2> terms_state.log
+python search_google_trends.py terms state 2> terms_state.log 2>terms_state.log
